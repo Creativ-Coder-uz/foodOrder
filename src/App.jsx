504cd866efeb820130fb30b825a38cb7a29757eb
Asliddin Choriyev <asliddinchoriyev0828@gmail.com>
2024-01-18 11:47:21 +0500

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,11 @@ import "./App.css";
 import Layout from "./components/Layout/Layout";
 import AllFoods from "./components/UI/AllFoods/AllFoods";
 import Context from "./context/Context";
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from "./components/Pages/NotFound/NotFound";
 import Login from "./components/Pages/Login/Login";
-import Header from "./components/Layout/Header/Header";
-import { element } from "prop-types";
-
 function App() {
-  const ctx = useContext(Context);
   const [orders, setOrders] = useState([]);
   const [openModal, setOpenModal] = useState(false);
   const [ordersCount, setOrdersCount] = useState(1);
